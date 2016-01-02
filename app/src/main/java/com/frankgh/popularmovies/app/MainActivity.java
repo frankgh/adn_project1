3ae1a133d51535b771e834c917cd84064dc69306
frank.guerrero <frank.guerrero@gmail.com>
2016-01-02 07:28:05 -0800

    Added tablet layout > 7"

--- app/src/main/java/com/frankgh/popularmovies/app/MainActivity.java
+++ app/src/main/java/com/frankgh/popularmovies/app/MainActivity.java
@@ -13,9 +13,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String DETAIL_FRAGMENT_TAG = "MOVIE_DETAIL_FRAGMENT";
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+
+        mTwoPane = findViewById(R.id.fragment_movie_detail) != null;
+
+        if (mTwoPane && savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_movie_detail, new MovieDetailActivityFragment(), DETAIL_FRAGMENT_TAG)
+                    .commit();
+        }
     }
 
     @Override
